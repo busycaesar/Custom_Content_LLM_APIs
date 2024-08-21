f05debb376b1a68a24dc6d40f29b022c5c1c0a82
Dev Shah <busycaesar@gmail.com>
2024-08-21 12:59:02 -0400

    Init new node project

---
+++ src/routes/api/index.js
@@ -0,0 +1,8 @@
+const router = require("express").Router();
+const response = require("../response");
+
+router.get("/", async (req, res) => {
+  res.status(200).json(response(true, "Example Route"));
+});
+
+module.exports = router;
