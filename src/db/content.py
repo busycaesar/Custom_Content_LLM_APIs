0a0bca6f9314b37174b95b32d955727e46f0373d
Dev Shah <busycaesar@gmail.com>
2024-11-17 16:55:00 -0500

    Feat: Store the content in vector db

--- src/db/content.py
+++ src/db/content.py
@@ -1,7 +1,31 @@
-async def add_new_content(content):
-    # Store the content in a vector database.
-    # Return unique key of the content.
-    return 000
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+import os
+from dotenv import load_dotenv
+from langchain.schema import Document
+from langchain_community.vectorstores import FAISS
+
+load_dotenv()
+
+gemini_api_keys = os.getenv("GEMINI_API_KEY")
+db = None
+
+async def add_new_content(content: str):
+    global db
+
+    # Convert the text into Document type.
+    content = Document(page_content=content)
+    
+    # Initiate the text splitter.
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
+    
+    # Using the text splitter, split the content into chunks by passing the converted content.
+    documents = text_splitter.split_documents([content])
+    
+    # Initiate an embedding model.
+    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001", google_api_key=gemini_api_keys)
+
+    db = FAISS.from_documents(documents, embedding=embeddings)
 
 async def get_content():
     
@@ -16,6 +40,13 @@ async def delete_content():
     return 000;
 
 async def get_relevant_chunk(prompt):
+    global db
     
-    relevant_chunk = ""
+    if db is None: 
+        print("No relevant content")
+        return "No relevant content"
+
+    docs = db.similarity_search(prompt, k=4)
+    relevant_chunk = " ".join([d.page_content for d in docs])
+
     return relevant_chunk
